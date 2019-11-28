8fd1db666c279e67e9710a486b557c728013927e
Liuwei <542544088@qq.com>
2019-11-28 11:00:26 +0800

    rotate

--- Solution.java
+++ Solution.java
@@ -2,8 +2,25 @@
 
 public class Solution {
     public static void main(String[] args) {
-        int[] a = new int[]{1, 1, 2};
-        removeDuplicates(a);
+//        int[] arr = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+//        removeDuplicates(arr);
+
+
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate(arr, 3);
+    }
+
+    private static void rotate(int[] nums, int k) {
+        int temp, pre;
+        for (int i = 0; i < k; i++) {
+            pre = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = pre;
+                pre = temp;
+            }
+        }
+        printArray(nums);
     }
 
     private static int removeDuplicates(int[] nums) {
@@ -13,11 +30,23 @@ public class Solution {
         int i = 0;
 
         for (int j = 0; j < nums.length; j++) {
-            if(nums[j] != nums[i]){
+            if (nums[j] != nums[i]) {
                 i++;
-                nums[i]= nums[j];
+                nums[i] = nums[j];
             }
         }
-        return i+1;
+        return i + 1;
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ",");
+            }
+        }
+        System.out.print("]");
     }
 }
